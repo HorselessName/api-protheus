4fc6d7888b0aa3d8434164f3c3b86ddac8e4103b
HorselessName <raulpotter11@gmail.com>
2024-02-23 14:00:42 -0300

    GDM-114 # Criação dos Filtros na API para Informações sobre o Usuário.

--- app/models/Usuario.py
+++ app/models/Usuario.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from db_context import db_sql
 
 
@@ -11,3 +11,20 @@ class Usuario(db_sql.Model):
     usuario_login: Mapped[str] = mapped_column("USR_CODIGO", db_sql.String, unique=True, nullable=False)
     usuario_nome: Mapped[str] = mapped_column("USR_NOME", db_sql.String, nullable=False)
     usuario_bloqueado: Mapped[str] = mapped_column("USR_MSBLQL", db_sql.String, nullable=False)
+
+    # Relacionamento N..1 com o Usuario: Um Usuario pode ter muitas filiais.
+    # Backref: Cria uma referência na classe UsuarioFilial para deixar acessar o Usuario pela UsuarioFilial.
+    # Exemplo: uma_filial = sessao.query(UsuarioFilial).first()
+
+    filiais: Mapped[list["UsuarioFilial"]] = relationship("UsuarioFilial", backref="usuario", lazy='joined')
+
+
+class UsuarioFilial(db_sql.Model):
+    __tablename__ = 'SYS_USR_FILIAL'
+
+    usuario_filial: Mapped[str] = mapped_column("USR_FILIAL", db_sql.String, primary_key=True, nullable=False)
+    usuario_filial_grupo: Mapped[str] = mapped_column("USR_GRPEMP", db_sql.String, nullable=False)
+    usuario_acesso: Mapped[str] = mapped_column("USR_ACESSO", db_sql.String, nullable=False)
+
+    # Relacionamento para determinar a condição de `join` entre as tabelas relacionadas.
+    usuario_codigo: Mapped[str] = mapped_column("USR_ID", db_sql.String, db_sql.ForeignKey(Usuario.usuario_codigo))
--- app/schemas/UsuarioSchema.py
+++ app/schemas/UsuarioSchema.py
@@ -1,8 +1,24 @@
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
-from models import Usuario
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, auto_field
+from models import Usuario, UsuarioFilial
+from marshmallow import fields
+
+
+class UsuarioFilialSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = UsuarioFilial
+        load_instance = True
+
+    # Especificar campos personalizados.
+    usuario_filial = auto_field()
+    usuario_filial_grupo = auto_field()
+    usuario_acesso = auto_field()
 
 
 class UsuarioSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = Usuario
         load_instance = True
+        include_fk = True
+
+    # When a class needs to be instanced, use `Class()` instead of `Class`
+    filiais = fields.Nested(UsuarioFilialSchema(), many=True)
